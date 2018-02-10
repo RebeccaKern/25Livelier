dc4f191912a4eb5e1986ed29af1e214dbe9d392a
Jordan Stapinski <jstapins@andrew.cmu.edu>
2018-02-10 11:35:25 -0500

    Completed event mailer

--- app/mailers/confirm_mailer.rb
+++ app/mailers/confirm_mailer.rb
@@ -1,2 +1,6 @@
 class ConfirmMailer < ApplicationMailer
+  def new_event_msg(event)
+    @event = event
+    mail(:to => "jstapins@andrew.cmu.edu", :subject => "Event Confirmation")
+  end
 end
--- config/initializers/setup_mailer.rb
+++ config/initializers/setup_mailer.rb
@@ -1,7 +1,7 @@
   ActionMailer::Base.smtp_settings = {  
     :address              => "smtp.mail.yahoo.com",       
     :port                 => 587,                         
-    :user_name            => ENV["TEMP_email"],  # ENV['YAHOO_ADDR']
+    :user_name            => ENV["TEMP_EMAIL"],  # ENV['YAHOO_ADDR']
     :password             => ENV["TEMP_PWD"],       # ENV['YAHOO_PWD']
     :authentication       => "plain",  
     :enable_starttls_auto => true  
